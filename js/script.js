326b1c5b1381192a38e5d5eb8f49be786db53311
Shigatsi <shigatsii@gmail.com>
2020-04-19 16:29:30 +0300

    feature/popup_submit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const editButton = document.querySelector('.profile__editButton');
 const closeButton = document.querySelector('.popup__closeButton');
-console.log(closeButton);
 let popupStatus = document.querySelector('.popup');
+let inputName = document.querySelector('.popup__name');
+let inputLifestyle = document.querySelector('.popup__lifestyle');
+let Name = document.querySelector('.profile__name');
+let Lifestyle = document.querySelector('.profile__lifestyle');
+
 
 popupStatus.classList.add('popup_staus_closed');
 
@@ -9,6 +13,8 @@ function showPopup() {
   console.log('Мы кликнули по editButton');
   popupStatus.classList.remove('popup_staus_closed');
   popupStatus.classList.add('popup_staus_opened');
+  inputName.value = Name.textContent;
+  inputLifestyle.value = Lifestyle.textContent;
 }
 
 function hidePopup(){
@@ -17,5 +23,17 @@ function hidePopup(){
   popupStatus.classList.remove('popup_staus_opened');
 };
 
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  Name.textContent = inputName.value;
+  Lifestyle.textContent = inputLifestyle.value;
+  popupStatus.classList.add('popup_staus_closed');
+  popupStatus.classList.remove('popup_staus_opened');
+
+
+};
+
+popupStatus.addEventListener('submit', formSubmitHandler);
 editButton.addEventListener('click', showPopup);
 closeButton.addEventListener('click', hidePopup);
