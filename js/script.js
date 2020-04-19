3d1b5f61746a9de870845d5cd432738bd8b3d04f
Shigatsi <shigatsii@gmail.com>
2020-04-19 17:26:14 +0300

    feature/script_overlay

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const editButton = document.querySelector('.profile__editButton');
 const closeButton = document.querySelector('.popup__closeButton');
+const overlay = document.querySelector('.overlay');
 let popupStatus = document.querySelector('.popup');
 let inputName = document.querySelector('.popup__name');
 let inputLifestyle = document.querySelector('.popup__lifestyle');
@@ -7,20 +8,25 @@ let Name = document.querySelector('.profile__name');
 let Lifestyle = document.querySelector('.profile__lifestyle');
 
 
-popupStatus.classList.add('popup_staus_closed');
+popupStatus.classList.add('popup_status_closed');
+overlay.classList.add('overlay_status_hide');
 
 function showPopup() {
   console.log('Мы кликнули по editButton');
-  popupStatus.classList.remove('popup_staus_closed');
-  popupStatus.classList.add('popup_staus_opened');
+  popupStatus.classList.remove('popup_status_closed');
+  popupStatus.classList.add('popup_status_opened');
+  overlay.classList.remove('overlay_status_hide');
+  overlay.classList.add('overlay_staus_seen');
   inputName.value = Name.textContent;
   inputLifestyle.value = Lifestyle.textContent;
 }
 
 function hidePopup(){
   console.log('Мы кликнули по closeButton');
-  popupStatus.classList.add('popup_staus_closed');
-  popupStatus.classList.remove('popup_staus_opened');
+  popupStatus.classList.add('popup_status_closed');
+  popupStatus.classList.remove('popup_status_opened');
+  overlay.classList.add('overlay_status_hide');
+  overlay.classList.remove('overlay_status_seen');
 };
 
 
@@ -28,10 +34,10 @@ function formSubmitHandler (evt) {
   evt.preventDefault();
   Name.textContent = inputName.value;
   Lifestyle.textContent = inputLifestyle.value;
-  popupStatus.classList.add('popup_staus_closed');
-  popupStatus.classList.remove('popup_staus_opened');
-
-
+  popupStatus.classList.add('popup_status_closed');
+  popupStatus.classList.remove('popup_status_opened');
+  overlay.classList.add('overlay_status_hide');
+  overlay.classList.remove('overlay_status_seen');
 };
 
 popupStatus.addEventListener('submit', formSubmitHandler);
