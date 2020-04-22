4ce5f682098b2934436d7f085da2d9bf50890534
Shigatsi <shigatsii@gmail.com>
2020-04-22 21:24:06 +0300

    bugfix/popup

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__closeButton');
+const closeButton = document.querySelector('.popup__close-button');
 const overlay = document.querySelector('.overlay');
 // let form = document.querySelector('.popup__form');
 let popupStatus = document.querySelector('.popup');
-let inputName = document.querySelector('.popup__name');
-let inputLifestyle = document.querySelector('.popup__lifestyle');
+let inputName = document.getElementById('popup_name');
+let inputLifestyle = document.getElementById('popup_lifestyle');
 let Name = document.querySelector('.profile__name');
 let Lifestyle = document.querySelector('.profile__lifestyle');
 
