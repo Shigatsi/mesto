048797318c610f6c40ecf27024cace91720d142d
Shigatsi <shigatsii@gmail.com>
2020-05-06 10:59:04 +0300

    feature/cardfromboxjs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
 const overlay = document.querySelector('.overlay');
+//сюда будут добавляться карточки
+const elemSection = document.querySelector('.elements');
 // Находим форму в DOM
 let popupStatus = document.querySelector('.popup');
 let inputName = document.getElementById('popup_name');
@@ -8,6 +10,54 @@ let inputLifestyle = document.getElementById('popup_lifestyle');
 let profileName = document.querySelector('.profile__name');
 let profileLifestyle = document.querySelector('.profile__lifestyle');
 
+//"коробка" с карточками изначальными
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+      alt: 'горная долина'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+      alt: 'лесное озеро зимой'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+      alt: 'панельные дома'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+      alt: 'сопка в далеке'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+      alt: 'железная дорога через лес'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+      alt: 'крутой обрыв у замерзшего озера'
+  }
+];
+
+
+//функция для карточек из "коробки"
+function addCards (card){
+  const elementTemplate = document.querySelector('#element-template').content;
+  const cardElement = elementTemplate.cloneNode(true);
+  cardElement.querySelector('.elements__title').textContent = card.name;
+  cardElement.querySelector('.elements__image').src=card.link;
+  cardElement.querySelector('.elements__image').alt = card.alt;
+  elemSection.append(cardElement);
+}
+
+//вывод карточек на страницу
+
+initialCards.forEach(addCards);
 
 function showOverlay(){
   overlay.classList.remove('overlay_status_hide');
@@ -44,16 +94,7 @@ function formSubmitHandler (evt) {
   // Вставьте новые значения с помощью textContent
   profileName.textContent = inputName.value;
   profileLifestyle.textContent = inputLifestyle.value;
-<<<<<<< HEAD
-  popupStatus.classList.add('popup_status_closed');
-  popupStatus.classList.remove('popup_status_opened');
-  popupStatus.classList.add('popup_status_closed');
-  popupStatus.classList.remove('popup_status_opened');
-  overlay.classList.add('overlay_status_hide');
-  overlay.classList.remove('overlay_status_seen');
-=======
   hidePopup();
->>>>>>> bugfix
 };
 
 // Прикрепляем обработчик к форме:
