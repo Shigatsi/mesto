82f82773818289fdb64f25579fd5c4a4fa7fd677
Shigatsi <shigatsii@gmail.com>
2020-05-08 17:05:33 +0300

    feature/open_close_popupup_single_func

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
-const editButton = document.querySelector('.profile__edit-button');//кнопка редактирования профиля
-const overlay = document.querySelector('.overlay');//затемнение страницы при открытии формы
-const addCardButton = document.querySelector('.profile__add-button');//кнопка добавления новой формы
-const profileForm = document.querySelector('#edit-profile');//форма редактирования профиля
-const placeForm = document.querySelector('#edit-place');//форма редактирования места
 const elementTemplate = document.querySelector('#element-template').content;//темплейт в DOM вызов по ID
 const elemSection = document.querySelector('.elements');//сюда будут добавляться карточки
-let popupStatus = document.querySelector('.popup');// Находим форму в DOM
+
+const overlay = document.querySelector('.overlay');//затемнение страницы при открытии формы
+
+const profileForm = document.querySelector('#edit-profile');//форма редактирования профиля
+const editButton = document.querySelector('.profile__edit-button');//кнопка редактирования профиля
+const closeProfileFormBtn = document.querySelector('#profile-close');//кнопка закрытия формы ред профиля
+const submitProfileFormBtn = document.querySelector('#profile-submit');//кнопка сохранения изменений профиля
+
+const placeForm = document.querySelector('#edit-place');//форма добавления места
+const addCardButton = document.querySelector('.profile__add-button');//кнопка добавления нового места
+const closePlaceFormBtn = document.querySelector('#place-close');//кнопка закрытия формы добавления места
+const submitPlaceFormBtn = document.querySelector('#place-submit');//кнопка сохранения нового места
+
+const profileName = document.querySelector('.profile__name');//имя профиля
+const profileLifestyle = document.querySelector('.profile__lifestyle');//профессия профиля
 let inputName = document.getElementById('popup_name');//ввод имени профиля
 let inputLifestyle = document.getElementById('popup_lifestyle');// ввод професси в профиле
 let inputPlace = document.querySelector('#popup_place');//ввод названия места
@@ -57,7 +66,11 @@ function createCards (card){
   cardElement.querySelector('.elements__like-button').addEventListener('click',setLikeActive);
   //кнопка корзина.событие клик
   cardElement.querySelector('.elements__rbin-button').addEventListener('click',deletButtonHandler);
-  elemSection.append(cardElement);
+  //кнопка-изображение для открытия формы просмотра фото
+  // cardElement.querySelector('.elements__image').addEventListener('click', setOpenPlaceImageHandler);
+
+  elemSection.prepend(cardElement);//задать чтобы изначальные карточки загрпужались отдельно?! но как?
+  return cardElement;
 }
 
 //рендерим карточки
@@ -77,53 +90,47 @@ function setLikeActive(event){
 //delete
 function deletButtonHandler(event){
   console.log(event.target.closest('.elements__item'));
-  event.target.closest('.elements__item').remove();
+  event.target.closest('.elements__item').remove();//otvalbashki
 }
 
-//функция для карточек из "коробки"
-//ниже все работает (просто надо немного поэксперементировать)
-// function addCards (card){
-//   const elementTemplate = document.querySelector('#element-template').content;
-//   const cardElement = elementTemplate.cloneNode(true);
-//   cardElement.querySelector('.elements__title').textContent = card.name;
-//   cardElement.querySelector('.elements__image').src=card.link;
-//   cardElement.querySelector('.elements__image').alt = card.alt;
-//   //like
-//   cardElement.querySelector('.elements__like-button').addEventListener('click', function(event){
-//     event.target.classList.add('elements__like-button_active',true);
-//   })
-
-//   //delete
-//   cardElement.querySelector('.elements__rbin-button');
-//   addEventListener('click', function(evt){
-//     console.log(evt);
-//    event.target.parent.remove();
-//   })
-//   elemSection.append(cardElement);
-// }
-
-// //вывод карточек на страницу
-// initialCards.forEach(addCards);
-
-
-
-
-
+//показать затемнение
 function showOverlay(){
   overlay.classList.remove('overlay_status_hide');
   overlay.classList.add('overlay_staus_seen');
 };
 
+//скрыть затемнение
 function hideOverlay(){
   overlay.classList.add('overlay_status_hide');
   overlay.classList.remove('overlay_status_seen');
 };
 
-function setClosePopupHandler(){
-  hideOverlay();
-  popupStatus.classList.add('popup_status_closed');
-  popupStatus.classList.remove('popup_status_opened');
+//функция открытия форм
+function openPopup(btn){
+  showOverlay()
+  if (btn.classList.contains('profile__edit-button')){
+    inputName.value = profileName.textContent;
+    inputLifestyle.value = profileLifestyle.textContent;
+    profileForm.classList.add('popup_opened');
+
+  }
+  if (btn.classList.contains('profile__add-button')){
+    placeForm.classList.add('popup_opened');
+  }
 };
+
+//функция закрытия форм
+function ClosePopup(btn){
+  hideOverlay();
+  if(btn.id = 'profile-close'){
+    profileForm.classList.remove('popup_opened');
+  }
+  if(btn.id='place-close'){
+    placeForm.classList.remove('popup_opened');
+  }
+}
+
+
 
 // // Обработчик «отправки» формы, хотя пока
 // // она никуда отправляться не будет
@@ -136,20 +143,12 @@ function setClosePopupHandler(){
 //   // Вставьте новые значения с помощью textContent
 //   profileName.textContent = inputName.value;
 //   profileLifestyle.textContent = inputLifestyle.value;
-//   setClosePopupHandler();
+//   ClosePopup(evt.target);
 // };
 
-// function setOpenProfileFormHandler(){
-//   showOverlay();
-//   profileForm.classList.add('popup_status_opened');
-//   inputName.value = profileName.textContent;
-//   inputLifestyle.value = profileLifestyle.textContent;
-// }
 
-function setOpenPlaceFormHandler(){
-  showOverlay()
-  placeForm.classList.add('popup_status_opened');
-}
+
+
 
 //обработчик события, добавляем новые карточки it's a life!!!!!!
 function formPlaceSubmitHandler (evt) {
@@ -159,18 +158,29 @@ function formPlaceSubmitHandler (evt) {
     elemSection.prepend(card);
     inputPlace.value = '';
     inputLink.value = '';
-    setClosePopupHandler();
+    ClosePopup(evt.target);
 };
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-// popupStatus.addEventListener('submit', formSubmitHandler);
-popupStatus.addEventListener('submit', formPlaceSubmitHandler);
-//editButton.addEventListener('click',setOpenProfileFormHandler);
-addCardButton.addEventListener('click', setOpenPlaceFormHandler);
-document.querySelectorAll('.popup__close-button').forEach(function(closeButton){
-  closeButton.addEventListener('click', function(event){
-    console.log(event);
-    event.setClosePopupHandler();
-  });
-});
+//открытие формы "Редактировать профиль"
+editButton.addEventListener('click', function (event) {
+  console.log(event.target);
+  openPopup(event.target);
+})
+
+//открытие формы "Новое место"
+addCardButton.addEventListener('click', function (event) {
+  console.log(event.target);
+  openPopup(event.target);
+})
+
+//закрытие формы "Редактировать профиль"
+closeProfileFormBtn.addEventListener('click', function (event) {
+  console.log(event.target);
+  ClosePopup(event.target);
+})
+
+//закрытие формы "Новое место"
+closePlaceFormBtn.addEventListener('click', function (event) {
+  console.log(event.target);
+  ClosePopup(event.target);
+})
